00b882c62c4277e474bd3ed85280be50628c885a
Shashwat Raj <46080469+RajShashwat@users.noreply.github.com>
2019-05-30 00:36:16 +0530

    Multiple Inheritance

--- Inheritance/Multiple.cpp
+++ Inheritance/Multiple.cpp
@@ -1,5 +1,4 @@
 //Created by Shashwat Raj
-#include<iostream>//Created by Shashwat Raj
 #include<iostream>
 using namespace std;
 //Base Class
@@ -27,38 +26,6 @@ public:
     eBook()
     {
         cout<<"Constructor of eBook"<<endl;
-    }
-};
-
-int main()
-{
-    eBook eB;
-}
-
-using namespace std;
-
-class Book
-{
-public:
-    Book()
-    {
-        cout<<"Base Class Constructor of Book"<<endl;
-    }
-};
-class Comic
-{
-public:
-    Comic()
-    {
-        cout<<"Base class Constructor of Comic"<<endl;
-    }
-};
-class eBook:public Book, public Comic
-{
-public:
-    eBook()
-    {
-        cout<<"Derived Class Constructor of eBook"<<endl;
     }
 };
 
